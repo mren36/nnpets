7fc5999f1a8cce3ed3b3eadf2bd14058e7fbe096
Daniel Bessonov <danielbessonov@daniels-mbp.lan>
2019-01-06 19:15:36 -0800

    update ui again

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
-import { Badge, ButtonGroup, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Badge, ButtonGroup, Button, Form, FormGroup, Label, Input, CustomInput } from 'reactstrap';
 
 class Post extends Component {
 
@@ -15,16 +15,35 @@ class Post extends Component {
     return (
       <Fragment>
         { this.state.post && <div className="position-relative">
-          <span className="d-block pb-2 mb-0 h6 text-uppercase text-info font-weight-bold" tyle={{margin: '5px'}}>
+          <span className="d-block pb-2 mb-0 h6 text-uppercase text-info font-weight-bold" style={{margin: '5px'}}>
             Upgrades for your Network
-            <Badge pill color="success" className="text-uppercase px-2 py-1 ml-3 mb-1 align-middle" style={{ fontSize: '0.75rem' }}>50 Points</Badge>
+            <Badge pill color="success" className="text-uppercase px-2 py-1 ml-3 mb-1 align-middle" style={{ fontSize: '0.75rem' }}>__ Points</Badge>
           </span>
-          <Form inline style={{margin: '5px'}}>
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="exampleEmail" className="mr-sm-2">Layers</Label>
-              <Input type="text" name="layers" id="layerInput" placeholder="5" />
-            </FormGroup>
-          </Form>
+        <FormGroup style={{margin: '5px'}}>
+          <Label for="exampleCustomSelect" style={{margin: '5px'}}>Number of Layers</Label>
+          <CustomInput type="select" id="exampleCustomSelect" name="customSelect" >
+            <option value="">Select</option>
+            <option>+1 (10 points)</option>
+            <option>+2 (20 points)</option>
+            <option>+3 (30 points)</option>
+            <option>+4 (40 points)</option>
+            <option>+5 (50 points)</option>
+          </CustomInput>
+        </FormGroup>
+
+        <FormGroup style={{margin: '5px'}}>
+          <Label for="exampleCustomSelect" style={{margin: '5px'}}>Some Other Parameter</Label>
+          <CustomInput type="select" id="exampleCustomSelect" name="customSelect" >
+            <option value="">Select</option>
+            <option>+1</option>
+            <option>+2</option>
+            <option>+3</option>
+            <option>+4</option>
+            <option>+5</option>
+          </CustomInput>
+        </FormGroup>
+
+        <Button color="success" style={{margin: '5px'}}>Send to Gym</Button>
 
         </div> 
       }
--- src/components/SideCard.js
+++ src/components/SideCard.js
@@ -13,6 +13,7 @@ const SideCard = () => (
       <CardImg top width="100%" src={BANNER} alt="banner" />
       <CardBody>
         <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary" style={{margin: '5px'}}>Image #120</CardTitle>
+        <CardSubtitle className="text-secondary mb-3 font-weight-light text-uppercase" style={{ fontSize: '0.8rem', margin: '5px' }}>119 seconds remaining</CardSubtitle>
         <CardText className="text-secondary mb-4" style={{ fontSize: '0.75rem', margin: '5px'}}>Please classify this animal as either a dog or a cat.</CardText>
         <ButtonGroup vertical>
           <Button color="success" className="font-weight-bold" style={{margin: '5px'}}>Dog</Button>
